1504dde2d6ff9a154122acbfa36a8133360218e9
pankajmishra <seepankaj.mishra@gmail.com>
2016-05-15 18:36:40 +0530

    update

--- CodingStuffs/src/com/codility/array/StreamingSearch.java
+++ CodingStuffs/src/com/codility/array/StreamingSearch.java
@@ -5,21 +5,18 @@ public class StreamingSearch {
 	public static void main(String[] args) {
 		String str = "mycatiscatcat";
 		int i = 0;
-		String word = "cat";
-		StringBuilder searchWord = new StringBuilder("abc");
-		System.out.println(searchWord);
-		System.out.println(searchWord.reverse());
-		System.out.println(searchWord);
+		String searchPattern = "cat";
+		StringBuilder searchString = new StringBuilder();
 		char[] strChar = str.toCharArray();
 		int count = 0;
 
 		while (i < strChar.length) {
-			searchWord.append(strChar[i]);
-			if (searchWord.length() == word.length()) {
-				if (searchWord.indexOf(word) == 0) {
+			searchString.append(strChar[i]);
+			if (searchString.length() == searchPattern.length()) {
+				if (searchString.indexOf(searchPattern) == 0) {
 					count++;
 				}
-				searchWord.deleteCharAt(0);
+				searchString.deleteCharAt(0);
 			}
 			i++;
 		}
